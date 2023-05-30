5b71804b23707c47eba3b2b2c30a0017696b381d
Josilene Silva <josilenevitoriasilva@gmail.com>
2023-05-29 20:19:44 -0400

    feature: adiciona usuario admin

---
+++ src/main/resources/db/migration/V8__Adicionando_Administrador.sql
@@ -0,0 +1,3 @@
+INSERT INTO `reserva_data`.`pessoa`
+(`cpf`,`data_nascimento`,`nome`,`siape`,`numero`,`tipo`,`tipo_perfil`,`setor_id`,`email`,`senha`,`status`,`codigo_ativacao`)
+VALUES(NULL,NULL,"Administrador","1111111",NULL,NULL,"ADMIN",1,"comunicacaointerna@gmail.com","$2a$10$u3Ibg9AWGcbDMn5RCDX21ezKFunqxOB5BBc9pnL2Y/kIlQ4uq4JI.","ATIVADA",NULL);
