adebd3adbbb566d2d8d680974b7487c39e7445bc
João da Mata <jlfilho@users.noreply.github.com>
2023-09-21 10:39:41 -0400

    Update Constantes.java

--- src/main/java/reserva_api/utils/Constantes.java
+++ src/main/java/reserva_api/utils/Constantes.java
@@ -6,7 +6,7 @@ import reserva_api.models.PessoaModel;
 import java.text.MessageFormat;
 
 public class Constantes {
-    public static String urlFront = "http://10.82.200.11:3010"; // TODO: adicionar url do front em prod
+    public static String urlFront = "http://reserva.icet.ufam.edu.br:3010"; // TODO: adicionar url do front em prod
     public static String adminEmail = "patrimonioicet@ufam.edu.br";
 
 }
